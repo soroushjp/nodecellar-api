ede5b640300c2f32bcc2944c4ac141909569f7a0
Soroush Pour <me@soroushjp.com>
2013-06-13 15:59:09 -0700

    Finished REST API

--- routes/wines.js
+++ routes/wines.js
@@ -19,25 +19,72 @@ db.open(function(err, db) {
 	}
 });
 
-
-exports.findById = function(req,res) {
-	res.send({id:req.params.id,
-			name:"The Name",
-			description:"The Description"
-		});
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving wine: ' + id);
+    db.collection('wines', function(err, collection) {
+        collection.findOne({"_id":new BSON.ObjectID(id)}, function(err, item) {
+             if(err) res.send(err);
+             else res.send(item);
+        });
+    });
 };
 
 exports.findAll = function(req,res) {
-	res.send([{name:'wine1'},
-				 {name:"wine2"},
-				  {name:"wine3"}]);
+	db.collection('wines', function(err,collection) {
+		collection.find().toArray(function(err,items) {
+	         if(err) res.send(err);
+	         else res.send(items);
+		});
+	});
 };
 
-exports.addWine = function(req,res) {};
+exports.addWine = function(req,res) {
+	var wine = req.body;
+	console.log('Adding wine: ' + JSON.stringify(wine));
+	db.collection('wines', function(err,collection) {
+		collection.insert(wine, {safe:true}, function(err, result) {
+			if(err) res.send({'error':'An error has occured'});
+			else {
+				console.log('Success: ' + JSON.stringify(result[0]));
+				res.send(result[0]);
+			}
+		});
+	});
+};
 
-exports.updateWine = function(req,res) {};
+exports.updateWine = function(req, res) {
+	var id = req.params.id;
+	var wine = req.body;
+	console.log('Updating wine with id: ' + id);
+	db.collection('wines', function(err, collection) {
+		collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err,result) {
+			if(err) {
+				console.log('Error updating wine: ' + err);
+				res.send({'error':'An error has occured'});
+			} else {
+				console.log(result + 'document(s) updated');
+				res.send(wine);
+			}
+		});
+	});
+}
 
-exports.deleteWine = function(req,res) {};
+exports.deleteWine = function(req,res) {
+	var id = req.params.id;
+	console.log('Deleting wine id: ' + id);
+	db.collection('wines', function(err, collection) {
+		collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+			if(err) {
+				console.log('Error removing wine: ' + err);
+				res.send({'error':err});
+			} else {
+				console.log(result + ' document(s) deleted');
+				res.send(req.body);
+			}
+		});
+	});
+}
 
 var populateDB = function() {
 
